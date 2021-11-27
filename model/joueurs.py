37c5d349643318b7c41a55af52c83c1fc82a21f5
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2021-11-27 21:30:30 +0100

    class player modifier, avec serialisation ajouté

--- model/joueurs.py
+++ model/joueurs.py
@@ -1,7 +1,34 @@
-class joueurs:
-    def __init__(self):
+class player:
+
+    def __init__(self, nom, prenom, DateNaissance, sexe, classement):
+        self.id = 0
         self.nom = nom
         self.prenom = prenom
         self.DateNaissance = DateNaissance
         self.sexe = sexe
         self.classement = classement
+        self.matchs = []
+
+    def show(self):
+        print("id" + str(self.id))
+        print("nom:" + self.nom)
+        print("prenom:" + self.prenom)
+        print("date de naissance:" + self.DateNaissance)
+        print("sexe:" + self.sexe)
+        print("classement:" + self.classement)
+
+    #def sauve(self):
+        #db.insert({'id':self.id, 'nom': self.nom, 'prenom': self.prenom, 'date de naissance':self.DateNaissance, 'sexe': self.sexe, 'classement': self.classement})
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'nom': self.nom,
+            'prenom': self.prenom,
+            'date de naissance': self.DateNaissance,
+            'sexe': self.sexe,
+            'classement': self.classement
+        }
+
+    def sauve(self):
+        db.insert(self.serialize())
