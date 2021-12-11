29420c2fe8fb7a586ba38d60a30a5c4051cefb17
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2021-12-11 22:04:48 +0100

    fonction de serealization et de concaténation des entrées

--- model/tournoi.py
+++ model/tournoi.py
@@ -1,8 +1,31 @@
+from tinydb import TinyDB, Query, where
+db = TinyDB('tournoi.json')
+
+
 class Tournoi:
-    def __init__(self):
+    def __init__(self, nom, lieu, date, tournees, joueurs):
         self.nom = nom
         self.lieu = lieu
         self.date = date
         self.tours = 4 # le nombre de tours par défaut est de 4
         self.tournees = tournees
         self.joueurs = joueurs
+
+    def show(self):
+        print("nom:" + self.nom)
+        print("lieu:" + self.lieu)
+        print("date:" + self.date)
+        print("tours:" + self.tours)
+        print("joueurs:" + self.joueurs)
+
+    def serialize(self):
+        return {
+            'nom': self.nom,
+            'lieu': self.lieu,
+            'date ': self.date,
+            'tours': self.tours,
+            'joueurs': self.joueurs
+        }
+
+    def sauve(self):
+        db.insert(self.serialize())
