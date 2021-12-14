657b793f99bcfcbbe36ef66e9922104d1b46a3d7
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2021-12-14 22:49:07 +0100

    modif code et var en anglais

--- model/tournoi.py
+++ model/tournoi.py
@@ -3,29 +3,27 @@ db = TinyDB('tournoi.json')
 
 
 class Tournoi:
-    def __init__(self, nom, lieu, date, tournees, joueurs):
-        self.nom = nom
-        self.lieu = lieu
-        self.date = date
-        self.tours = 4 # le nombre de tours par défaut est de 4
-        self.tournees = tournees
-        self.joueurs = joueurs
+    def __init__(self, name, place, dated):
+        self.name = name # nom du tournoi
+        self.place = place # lieu du tournoi
+        self.dated = dated # date du tournoi
+        self.round = 4 # le nombre de tours par défaut est de 4
+        self.tours = [] # La liste des instances rondes.
+        self.player = [] # joueurs
 
     def show(self):
-        print("nom:" + self.nom)
-        print("lieu:" + self.lieu)
-        print("date:" + self.date)
-        print("tours:" + self.tours)
-        print("joueurs:" + self.joueurs)
+        print("name:" + self.name)
+        print("place:" + self.place)
+        print("dated:" + self.dated)
 
     def serialize(self):
         return {
-            'nom': self.nom,
-            'lieu': self.lieu,
-            'date ': self.date,
-            'tours': self.tours,
-            'joueurs': self.joueurs
+            'name': self.name,
+            'place': self.place,
+            'dated ': self.dated,
+            'round': self.round,
+            'player': self.player
         }
 
-    def sauve(self):
-        db.insert(self.serialize())
+    def save(self): # fonction de sauvegarde
+        db.insert(self.serialize()) # sauvegarde dans la db
