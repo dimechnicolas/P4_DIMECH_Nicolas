a5285c47a757f98a1b4766dd6341fffcadd889c7
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2021-12-21 20:56:31 +0100

    modification des print de façon plus détaillé

--- view/MainView.py
+++ view/MainView.py
@@ -2,13 +2,13 @@ class MainView:
     @staticmethod
     def display_menu():
         print("chess program")
-        print("1: joueur")
-        print("2: tournoi")
-        print("3: tours")
+        print("1: création d'un joueur")
+        print("2: création d'un tournoi")
+        print("3: création de tours")
         print("4: generer les pairs")
         print("5: mise à jours des classements")
-        print("6: rapports")
-        print("7: exite")
+        print("6: éditer les rapports")
+        print("7: quitter le programme")
         option = input("Votre choix ?")
         return option
 
