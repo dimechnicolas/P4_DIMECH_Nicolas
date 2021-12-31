a7223db3121fba4e7ee6b586488decf1b317fe89
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2021-12-31 18:58:41 +0100

    commentaire sur les dif fonctions

--- model/Round.py
+++ model/Round.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from model.Player import Player
 
 class Round:
-    def __init__(self):
+    def __init__(self, name=None):
         self.name = name
-        self.start_time = datetime.now().strftime('%d-%m%y %H%M%S')
+        self.start_time = datetime.now().strftime('%d%m%y %H%M%S')#%d day of month on two digits, %m two-digit month number, %y year in twodigits
         self.end_time = ''
         self.matches = []
 
@@ -13,3 +14,17 @@ class Round:
     def __str__(self): # renvoi une representation lisible par l'Homme/returns a human-readable representation
         return f'{self.name} ({self.start_time} - {self.end_time})'
 
+    def serialize(self):
+        return {
+        'name': self.name,
+        'start_time': self.start_time,
+        'end_time': self.end_time
+        }
+
+    def deserialize(self, r):
+        self.name = r['name']
+        self.start_time = r['start_time']
+        self.end_time = r['end_time']
+        return self
+
+        
