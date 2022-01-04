ffda53f90bceb26e980fee315772c2e7597a328d
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-01-04 23:25:04 +0100

    création fichier TournamentMenueView

--- view/MainView.py
+++ view/MainView.py
@@ -1,3 +1,6 @@
+"""vie entrée et sortie du menue avec gestion
+   menu entry and exit life with management"""
+
 class MainView:
     @staticmethod
     def display_menu():
@@ -5,7 +8,7 @@ class MainView:
         print("1: création d'un joueur")
         print("2: création d'un tournoi")
         print("3: création de tours")
-        print("4: generer les pairs")
+        print("4: générer les pairs")
         print("5: mise à jours des classements")
         print("6: éditer les rapports")
         print("7: quitter le programme")
---
+++ view/TournamentMenueView.py
@@ -0,0 +1,5 @@
+@staticmethod
+def tournament_menue():
+    print("1: créer un tour")
+    print("2: lancer le tour")
+    print("3: entrer les scor")
