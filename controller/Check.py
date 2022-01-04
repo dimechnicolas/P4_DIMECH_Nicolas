304acffe39192e2b91a083e07fcc86452bffe92e
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-01-04 23:26:29 +0100

    commentaire pour expliquer les regex

--- controller/Check.py
+++ controller/Check.py
@@ -8,27 +8,28 @@ class Check:
     @staticmethod
     def check_str(text):
         while True:
-            reponse = input(text)
-            reg_check_text = re.search('[[:alpha:]]', reponse)
-            if reg_check_text != 'True':
-                break
+            response = input(text)
+            match = re.search('^[[:alpha:] -]{3,}$', response)# verification des lettres au minima 3
+            if match:
+                return response
             print("vous avez rentrez une movaise valeure, merci de recommancez")
-        return reponse
+
 
     @staticmethod
     def check_sexe(text):
         while True:
-            reponse = input(text)
-            if reponse == "M" or reponse == "F":
-                break
+            response = input(text)
+            if response == "M" or response == "F":
+                return response
             print("vous devez rentrer M pour une homme et F pour une femme")
 
+
     @staticmethod
-    def check_int(int):
+    def check_date(text):
         while True:
-            reponse = input(int)
-            reg_check_int = re.search('\d', reponse)
-            if reponse != "True":
-                break
+            response = input(text)
+            match = re.search('^[\d]{2}\/[\d]{2}\/[\d]{4}$', response) #verification des dates avec /
+            if match:
+                return response
             print("merci de ne rentrer que des chiffres sans virgules")
-        return reponse
+
