f423f61f53e04888fc2c1d150059736de731ada7
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-01-09 22:11:42 +0100

    ajours changer de menu au menu tournament

--- view/TournamentMenueView.py
+++ view/TournamentMenueView.py
@@ -1,5 +1,12 @@
-@staticmethod
-def tournament_menue():
-    print("1: créer un tour")
-    print("2: lancer le tour")
-    print("3: entrer les scor")
+
+class tournamentMenue:
+    @staticmethod
+    def tournament_menue():
+        print("1: créer un tour")
+        print("2: lancer le tour")
+        print("3: entrer les scors")
+        print("4 retour au menu principal")
+
+    @staticmethod
+    def display_error(error):
+        print(error)
