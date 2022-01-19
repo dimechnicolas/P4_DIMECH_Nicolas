f607a543137930d542e657def1e9ba436d93cd1e
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-01-19 21:50:14 +0100

    modification des noms dans les vues du tournoi

--- view/TournamentMenuView.py
+++ view/TournamentMenuView.py
@@ -1,10 +1,11 @@
-class tournamentMenu:
+class tournamentMenuView:
     @staticmethod
-    def tournament_menu():
+    def view_menu():
         print("1: lancer le tour")
         print("2: terminer le tour")
         print("3: voir les rapports")
         print("4: retour au menu principal")
+        input("quel est votre choix?")
 
     @staticmethod
     def display_error(error):
