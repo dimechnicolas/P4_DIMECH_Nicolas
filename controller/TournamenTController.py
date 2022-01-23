3feb8c7e985adebef773db4e2721520cbe78e968
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-01-24 00:31:00 +0100

    ajours des fonctions début et fin d'un tour

--- controller/TournamenTController.py
+++ controller/TournamenTController.py
@@ -1,5 +1,5 @@
 from view.TournamentMenuView import tournamentMenuView
-from model.Tournament import Tournament
+from model.Tour import Tour
 
 class TournamentMenuController:
 
@@ -7,12 +7,21 @@ class TournamentMenuController:
 
     def Choise_controll(self):
         while 1:
-            option = self.tournament_menu.view_menu(), #renvoi à TournamentMenuView
+            option = self.tournament_menu.view_menu() #renvoi à TournamentMenuView
             if option == "1":
-                print("lancez le tour")
+                self.star_trick()
             elif option == "2":
-                print("terminer le tour")
+                self.stop_ride()
             elif option == "3":
                 print("vous entrez les scors")
             else:
                 self.tournament_menu.display_error("commande inconnue")
+
+    @staticmethod
+    def star_trick():
+        Tour.start_timer()
+
+    @staticmethod
+    def stop_ride():
+        Tour.end_timer()
+        Tour.timme_elapsed()
