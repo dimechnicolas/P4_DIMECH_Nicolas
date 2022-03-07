cda92c0f1b31f8981c99766eeec43e0a5425aa17
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-03-07 21:14:12 +0100

    débuggage

--- model/Tournament.py
+++ model/Tournament.py
@@ -3,35 +3,47 @@ db = TinyDB('tournoi.json')
 
 
 class Tournament:
-    def __init__(self, name, place, dated):
+    def __init__(self, name=None, place=None, dated=None, description=None, type_tournament=None):
         self.name = name # nom du tournoi
         self.place = place # lieu du tournoi
         self.dated = dated # date du tournoi
-        self.round = 4 # le nombre de tours par défaut est de 4
-        self.tours = [] # La liste des instances rondes.
+        self.description = description
+        self.type_tournament = type_tournament
+        self.rounds = 4 # le nombre de tours par défaut est de 4
+        self.rounds_list = [] # La liste des instances rondes, ajouter en séria et déséria
         self.player = [] # joueurs
 
-    def show(self):
-        print("name:" + self.name)
-        print("place:" + self.place)
-        print("dated:" + self.dated)
+    def __str__(self): # affiche la classe en string.
+        return f"{self.name}"
 
     def serialize(self):
         return {
             'name': self.name,
             'place': self.place,
-            'dated ': self.dated,
-            'round': self.round,
-            'player': self.player
+            'dated': self.dated,
+            'rounds': self.rounds,
+            'player': self.player,
+            'description': self.description,
+            'type_tournament': self.type_tournament
         }
 
     def deserialize(self, t):
         self.name = t['name']
         self.place = t['place']
         self.dated = t['dated']
-        self.round = t['round']
-        self.tours = t['tours']
+        self.rounds = t['rounds']
         self.player = t['player']
+        self.description= t['description']
+        self.type_tournament = t['type_tournament']
+        return self
+
 
     def save(self): # fonction de sauvegarde
         db.insert(self.serialize()) # sauvegarde dans la db
+
+    @staticmethod
+    def get_all():
+        tournament = []
+        for t in db.all():
+            tournament.append(Tournament().deserialize(t))
+        return tournament
