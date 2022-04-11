20c5c2420d82d351cc5a049ef8aa7ba975ebefa6
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-04-11 19:34:04 +0200

    création first et other paring ainsi que fin de tournoi

--- model/Round.py
+++ model/Round.py
@@ -19,14 +19,51 @@ class Round:
         'name': self.name,
         'start_time': self.start_time,
         'end_time': self.end_time,
-        'matches': self.matches
+        'matches': [([m[0][0].serialize(), m[0][1]], [m[1][0].serialize(), m[1][1]]) for m in self.matches]
         }
 
     def deserialize(self, r):
         self.name = r['name']
         self.start_time = r['start_time']
         self.end_time = r['end_time']
-        self.matches = r['matches']
+        self.matches = [([Player().deserialize(m[0][0]), m[0][1]],
+                         [Player().deserialize(m[1][0]), m[1][1]]) for m in r['matches']]
         return self
 
-        
+    def first_pairing(self, players):
+        sorted_list = sorted(players, key=lambda p: int(p.ranking), reverse=True)
+        print(sorted_list)
+        self.matches = [
+            ([sorted_list[0], -1], [sorted_list[4], -1]),
+            ([sorted_list[1], -1], [sorted_list[5], -1]),
+            ([sorted_list[2], -1], [sorted_list[6], -1]),
+            ([sorted_list[3], -1], [sorted_list[7], -1]),
+        ]
+
+    def other_pairing(self, players, rounds):
+        sorted_list = sorted(players, key=lambda p: (float(p.score), int(p.ranking)), reverse=True)
+        match_already_happened = False
+
+        for r in rounds:
+            for m in r.matches:
+                if (m[0][0].id == sorted_list[0].id and m[1][0].id == sorted_list[1].id) \
+                        or (m[1][0].id == sorted_list[0].id and m[0][0].id == sorted_list[1].id):
+                    match_already_happened = True
+
+        if match_already_happened:
+            self.matches = [
+                ([sorted_list[0], -1], [sorted_list[2], -1]),
+                ([sorted_list[1], -1], [sorted_list[3], -1]),
+                ([sorted_list[4], -1], [sorted_list[5], -1]),
+                ([sorted_list[6], -1], [sorted_list[7], -1]),
+            ]
+        else:
+            self.matches = [
+                ([sorted_list[0], -1], [sorted_list[1], -1]),
+                ([sorted_list[2], -1], [sorted_list[3], -1]),
+                ([sorted_list[4], -1], [sorted_list[5], -1]),
+                ([sorted_list[6], -1], [sorted_list[7], -1]),
+            ]
+
+    def stop_round(self):
+        self.end_time = datetime.now().strftime('%d-%m-%y %H:%M:%S')
