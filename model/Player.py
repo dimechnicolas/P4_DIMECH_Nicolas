af10b44442bee84b4679aea8e1287ca91007b262
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-05-10 20:26:42 +0200

    modif check_in_in_array pour gérer les erreurs

--- model/Player.py
+++ model/Player.py
@@ -1,16 +1,20 @@
 from tinydb import TinyDB, Query, where
-db = TinyDB('player.json')
 
 class Player:
 
     def __init__(self, first_name=None, name=None, birthday=None, sex=None, ranking=None):
-        self.id = 0
+        self.id = -1
         self.first_name = first_name
         self.name = name
         self.birthday = birthday
         self.sex = sex
         self.ranking = ranking
+        self.score = 0
         self.matchs = []
+        self.table = TinyDB('../player.json').table('Player')
+
+    def __str__(self):
+        return f'{self.first_name} {self.name}'
 
     def serialize(self):
         return {
@@ -19,25 +23,26 @@ class Player:
             'name': self.name,
             'birthday': self.birthday,
             'sex': self.sex,
-            'ranking': self.ranking
+            'ranking': self.ranking,
+            'score': self.score
         }
 
     def deserialize (self, p):
         self.id = p['id']
-        self.name = p['first_name']
+        self.first_name = p['first_name']
         self.name = p['name']
         self.birthday = p['birthday']
         self.sex = p['sex']
         self.ranking = p['ranking']
+        self.score = p['score']
         return self
 
     def save(self):
-        db.insert(self.serialize())
+        self.table.insert(self.serialize())
 
-    @staticmethod
-    def get_all():
+    def get_all(self):
         players = []
-        for p in db.all():
+        for p in self.table.all():
             players.append(Player().deserialize(p))
         return players
 
