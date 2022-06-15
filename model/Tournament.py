04e98ccb6826d154cc50232daf5c867de0844f90
Nicolas DIMECH <nicolasdimech.95200@gmail.com>
2022-06-15 21:20:47 +0200

    ajout de l'ID du tournoi + gestion de celui-ci dans la DB, serialisation et désérialisation de la liste des joueurs

--- model/Tournament.py
+++ model/Tournament.py
@@ -1,9 +1,14 @@
 from tinydb import TinyDB, Query, where
+
+from model.Player import Player
+from model.Round import Round
+
 db = TinyDB('tournoi.json')
 
 
 class Tournament:
     def __init__(self, name=None, place=None, dated=None, description=None, type_tournament=None):
+        self.id = -1
         self.name = name # nom du tournoi
         self.place = place # lieu du tournoi
         self.dated = dated # date du tournoi
@@ -17,29 +22,50 @@ class Tournament:
         return f"{self.name}"
 
     def serialize(self):
+        players = []
+        for p in self.player:
+            players.append(p.serialize())
+        rounds_list = []
+        for r in self.rounds_list:
+            rounds_list.append(r.serialize())
+
         return {
+            'id': self.id,
             'name': self.name,
             'place': self.place,
             'dated': self.dated,
             'rounds': self.rounds,
-            'player': self.player,
+            'rounds_list': rounds_list,
+            'player': players,
             'description': self.description,
             'type_tournament': self.type_tournament
         }
 
     def deserialize(self, t):
+        players = []
+        for p in t['player']:
+            players.append(Player().deserialize(p))
+        rounds_list = []
+        for r in t['rounds_list']:
+            rounds_list.append(Round().deserialize(r))
+        self.id = t['id']
         self.name = t['name']
         self.place = t['place']
         self.dated = t['dated']
         self.rounds = t['rounds']
-        self.player = t['player']
-        self.description= t['description']
+        self.rounds_list = rounds_list
+        self.player = players
+        self.description = t['description']
         self.type_tournament = t['type_tournament']
         return self
 
 
     def save(self): # fonction de sauvegarde
-        db.insert(self.serialize()) # sauvegarde dans la db
+        self.id = db.insert(self.serialize()) # sauvegarde dans la db
+        self.update()
+
+    def update(self):
+        db.update(self.serialize(), doc_ids=[self.id])
 
     @staticmethod
     def get_all():
